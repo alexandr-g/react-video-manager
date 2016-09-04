db7744582c62d2d52198e61daf00e77795a443e8
Alexandr <alexgrischuk@gmail.com>
2016-09-04 16:07:19 +0200

    Add call to youtube API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+import config from '../app.config';
 import SearchBar from './components/SearchBar';
 
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_API_KEY = config.YOUTUBE_API_KEY;
+
+YTSearch({key: YOUTUBE_API_KEY, term: 'react native'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
