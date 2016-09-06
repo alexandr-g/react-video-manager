bfab8901f5e8ac5c6bb3398480e5d57bfddb6228
Alexandr <alexgrischuk@gmail.com>
2016-09-06 20:56:28 +0200

    Refactor search function

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ class App extends Component {
        selectedVideo: null
      };
 
-    YTSearch({key: YOUTUBE_API_KEY, term: 'react native'}, (videos) => {
+     this.videoSearch('react native');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: YOUTUBE_API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -27,7 +31,7 @@ class App extends Component {
   render() {
     return (
         <div>
-          <SearchBar />
+          <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
           <VideoDetail video={this.state.selectedVideo} />
           <VideoList
             onVideoSelect={selectedVideo => this.setState({selectedVideo})}
